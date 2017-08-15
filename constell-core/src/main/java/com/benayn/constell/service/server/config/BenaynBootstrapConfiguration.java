1347b5fbae28138bbd98039740e95223ea442e47
yejiwei <yejiwei@u51.com>
2017-08-15 17:39:23 +0800

    bootstrap config

---
+++ constell-core/src/main/java/com/benayn/constell/service/server/config/BenaynBootstrapConfiguration.java
@@ -0,0 +1,8 @@
+package com.benayn.constell.service.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BenaynBootstrapConfiguration {
+
+}
