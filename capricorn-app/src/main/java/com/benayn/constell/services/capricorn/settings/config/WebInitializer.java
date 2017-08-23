7027388e5dece0fcb1e43fdffce1a1944c50a715
yejiwei <yejiwei@u51.com>
2017-08-23 11:33:34 +0800

    WebInitializer

---
+++ capricorn-app/src/main/java/com/benayn/constell/services/capricorn/settings/config/WebInitializer.java
@@ -0,0 +1,14 @@
+package com.benayn.constell.services.capricorn.settings.config;
+
+import com.benayn.constell.services.capricorn.CapricornApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CapricornApplication.class);
+    }
+
+}
