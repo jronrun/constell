764f84bfe0b06784185380a044db7847b0872630
yejiwei <yejiwei@u51.com>
2017-09-07 14:00:59 +0800

    thymeleaf layout

--- capricorn-app/src/main/java/com/benayn/constell/services/capricorn/settings/config/CapricornAppConfiguration.java
+++ capricorn-app/src/main/java/com/benayn/constell/services/capricorn/settings/config/CapricornAppConfiguration.java
@@ -4,6 +4,7 @@ import static com.benayn.constell.services.capricorn.settings.constant.Capricorn
 
 import com.benayn.constell.service.server.annotation.EnableBenaynSwagger;
 import com.benayn.constell.service.server.service.BenaynServiceInfo;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -15,6 +16,11 @@ public class CapricornAppConfiguration {
     @Bean
     public BenaynServiceInfo benaynServiceInfo() {
         return BenaynServiceInfo.of(new ApiInfoBuilder().title("Capricorn API Document").build(), SERVICE_NAME);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 
 }
