e865226411d9a9aa82f3635722da3e905522441f
yejiwei <yejiwei@u51.com>
2017-09-19 17:21:29 +0800

    manage index header

---
+++ capricorn-app/src/main/resources/static/manage/manage.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mgr = {
+    header: {
+        intl: function () {
+
+        }
+    },
+
+    initialize: function () {
+        mgr.header.intl();
+    }
+};
+
+$(function () { mgr.initialize(); });
