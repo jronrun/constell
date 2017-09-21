c0a47bc6d92869f7ccab9a5fa3360bd1081009a0
yejiwei <yejiwei@u51.com>
2017-09-21 16:26:08 +0800

    Capricorn Configurer

--- capricorn-app/src/main/java/com/benayn/constell/services/capricorn/settings/config/CapricornAppConfiguration.java
+++ capricorn-app/src/main/java/com/benayn/constell/services/capricorn/settings/config/CapricornAppConfiguration.java
@@ -4,8 +4,16 @@ import static com.benayn.constell.services.capricorn.settings.constant.Capricorn
 
 import com.benayn.constell.service.server.annotation.EnableBenaynSwagger;
 import com.benayn.constell.service.server.service.BenaynServiceInfo;
+import com.google.common.collect.Lists;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 
 @Configuration
@@ -15,6 +23,53 @@ public class CapricornAppConfiguration {
     @Bean
     public BenaynServiceInfo benaynServiceInfo() {
         return BenaynServiceInfo.of(new ApiInfoBuilder().title("Capricorn API Document").build(), SERVICE_NAME);
+    }
+
+    @Configuration
+    public class CapricornWebMvcConfigurer implements WebMvcConfigurer {
+
+        @Autowired
+        CapricornConfigurer capricornConfigurer;
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            capricornConfigurer.getResources()
+                .forEach(resource -> {
+                    registry
+                        .addResourceHandler(resource.getHandler())
+                        .addResourceLocations(resource.getLocation());
+                }
+            );
+        }
+
+    }
+
+    /*
+    capricorn:
+      configurer:
+        resources:
+        -
+         handler: /test1/**
+         location: classpath:/static/components/golgi/
+        -
+         handler: /test2/**
+         location: file:///Users/test/Documents/
+     */
+
+    @Data
+    @NoArgsConstructor
+    @Configuration
+    @ConfigurationProperties(prefix = "capricorn.configurer")
+    public static class CapricornConfigurer {
+
+        private List<StaticResourcesConfigurer> resources = Lists.newArrayList();
+
+        @Data
+        @NoArgsConstructor
+        static class StaticResourcesConfigurer {
+            private String handler;
+            private String location;
+        }
     }
 
 }
