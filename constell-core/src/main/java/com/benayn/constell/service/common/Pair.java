b0292da961740e2cd7dc03e5b17ac2b4addf1076
yejiwei <yejiwei@u51.com>
2017-10-27 20:49:03 +0800

    Pair

--- constell-core/src/main/java/com/benayn/constell/service/common/Pair.java
+++ constell-core/src/main/java/com/benayn/constell/service/common/Pair.java
@@ -2,9 +2,11 @@ package com.benayn.constell.service.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
 public class Pair<K, V> {
 
     private K key;
