1f7d20a12c3ea0ff410c476e63a572ea11b2791e
yejiwei <yejiwei@u51.com>
2018-01-24 16:14:16 +0800

    previews multi-tab

---
+++ capricorn-app/src/main/resources/static/entry/write.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var write = {};
+(function ($, root, register) {
+
+    var core = {
+        initialize: function () {
+
+        }
+    };
+
+    $.extend(register, {
+    });
+
+    $(function () {
+        core.initialize();
+    });
+
+})(jQuery, window, write);
+
+$(function () {
+    window.aa = comm.previews({
+        tabHead:true,
+        content: [
+            {
+                id: 'testa',
+                title: 'test a',
+                context: 'http://10.0.40.210:9060/swagger-ui.html#/'
+            },{
+                id: 'testb',
+                title: 'test b',
+                context: 'test b body'
+            }
+        ]});
+});
